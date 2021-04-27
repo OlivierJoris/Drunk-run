2cb5b1681ad08cf40277ec42cda980950bd3600d
Maxime Goffart <maximegoffart@icloud.com>
2021-04-27 11:08:38 +0200

    Game update

--- src/Game.cpp
+++ src/Game.cpp
@@ -7,13 +7,66 @@
 #include "Game.hpp"
 
 #include <memory>
+#include <vector>
+#include <list>
 
 using namespace std;
 
-Game::Game(){
+Game::Game(
+    unsigned int frameRate,
+    double clearingDistance,
+    double dangerousRate
+): frameRate{frameRate}, clearance{clearingDistance}, dangerousObstacleRate{dangerousRate}{
+
     state = make_shared<GameState>();
+    player = make_shared<Player>();
+    path = make_shared<Path>();
+
+    kerbs = vector<Kerb>();
+    kerbs.push_back(Kerb());
+    kerbs.push_back(Kerb());
+
+    obstacles = list<Object>();
+}
+
+void Game::draw(shared_ptr<Window> w){
+    // to be filled
 }
 
 shared_ptr<GameState> Game::get_game_state() const{
     return state;
+}
+
+shared_ptr<Player> Game::get_player() const{
+    return player;
+}
+
+shared_ptr<Path> Game::get_path() const{
+    return path;
+}
+
+const vector<Kerb>& Game::get_kerbs() const{
+    return kerbs;
+}
+
+const list<Object>& Game::get_obstacles() const{
+    return obstacles;
+}
+
+void Game::add_obstacle(Object& obstacle){
+    /* TO DO: modifies in order to keep the list sorted by distance.
+        Requires the Object class to be implemented. */
+    obstacles.push_back(obstacle);
+}
+
+unsigned int Game::get_frame_rate() const{
+    return frameRate;
+}
+
+double Game::get_clearance_dist() const{
+    return clearance;
+}
+
+double Game::get_distribution_dangerous() const{
+    return dangerousObstacleRate;
 }
--- src/Game.hpp
+++ src/Game.hpp
@@ -8,15 +8,46 @@
 #define __GAME__
 
 #include "GameState.hpp"
+#include "Window.hpp"
+#include "Object.hpp"
+#include "Player.hpp"
+#include "Path.hpp"
+#include "Kerb.hpp"
 
 #include <memory>
+#include <list>
+#include <vector>
 
 // Represents the game of drunk run.
 class Game{
 
 public:
-    // Constructor.
-    Game();
+    // Default frame rate for the game.
+    static const unsigned int DEFAULT_FRAME_RATE = 30;
+    // Default dangerous obstacles rate.
+    constexpr static const double DEFAULT_DANGEROUS_RATE = 0.3L;
+    // Default clearing distance before first obstacle.
+    constexpr static const double DEFAULT_CLEARANCE = 4.0L;
+
+    /*
+     * Constructor.
+     *
+     * @param frameRate Frame rate of the game.
+     * @param clearingDistance Clearing distance before first obstacle.
+     * @param dangerousRate Dangerous obstacles rate.
+     */
+    Game(
+        unsigned int frameRate = DEFAULT_FRAME_RATE,
+        double clearingDistance = DEFAULT_CLEARANCE,
+        double dangerousRate = DEFAULT_DANGEROUS_RATE
+    );
+
+    /*
+     * Draws the game on the given window.
+     *
+     * @param w The window on which to draw the game.
+     */
+    void draw(std::shared_ptr<Window> w);
 
     /*
      * Returns the state of the game.
@@ -25,9 +56,84 @@ public:
      */
     std::shared_ptr<GameState> get_game_state() const;
 
+    /*
+     * Returns the player of the game.
+     *
+     * @return Player of the game.
+     */
+    std::shared_ptr<Player> get_player() const;
+
+    /*
+     * Returns the path of the game.
+     *
+     * @return Path of the game.
+     */
+    std::shared_ptr<Path> get_path() const;
+
+    /*
+     * Returns the vector of kerbs of the game.
+     *
+     * @return Vector of kerbs.
+     */
+    const std::vector<Kerb>& get_kerbs() const;
+
+    /*
+     * Returns the list of obstacles of the game.
+     *
+     * @return List of obstacles as objects.
+     */
+    const std::list<Object>& get_obstacles() const;
+
+    /*
+     * Adds an obstacle to the game.
+     *
+     * @param obstacle Obstacle to add.
+     */
+    void add_obstacle(Object& obstacle);
+
+    /*
+     * Returns the fixed frame rate of the game.
+     *
+     * @return Fixed frame rate.
+     */
+    unsigned int get_frame_rate() const;
+
+    /*
+     * Returns the clearing distance of the game.
+     *
+     * @return Clearing distance.
+     */
+    double get_clearance_dist() const;
+
+    /*
+     * Returns the rate of dangerous obstacle.
+     *
+     * @return Rate of dangerous obstacle.
+     */
+    double get_distribution_dangerous() const;
+
 private:
     // State of the game.
     std::shared_ptr<GameState> state;
+
+    // Player of the game.
+    std::shared_ptr<Player> player;
+
+    // Path/road of the game.
+    std::shared_ptr<Path> path;
+
+    // Left & right kerb of the path/road.
+    std::vector<Kerb> kerbs;
+
+    // Obstacles on the path/road.
+    std::list<Object> obstacles;
+
+    // Frame rate of the game.
+    unsigned int frameRate;
+    // Waiting distance before generating the first object.
+    double clearance;
+    // Rate of dangerous obstacles.
+    double dangerousObstacleRate;
 
 };
 
