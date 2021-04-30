e0039f5aa8f99265018647d77701e5a87c86fd2c
Maxime Goffart <maximegoffart@icloud.com>
2021-04-30 14:53:59 +0200

    [rgb] default params constructor

--- src/RGBColor.cpp
+++ src/RGBColor.cpp
@@ -8,8 +8,6 @@
 
 #include <cstdint>
 
-RGBColor::RGBColor(){}
-
 RGBColor::RGBColor(uint8_t r, uint8_t g, uint8_t b){
     red = r;
     green = g;
--- src/RGBColor.hpp
+++ src/RGBColor.hpp
@@ -13,9 +13,6 @@
 class RGBColor{
 
 public:
-    // Constructor.
-    RGBColor();
-
     /*
      * Constructor.
      *
@@ -23,7 +20,7 @@ public:
      * @param g Value for green component.
      * @param b Value for blue component.
      */
-    RGBColor(uint8_t r, uint8_t g, uint8_t b);
+    RGBColor(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0);
 
     /*
      * Sets the value for the red component.
